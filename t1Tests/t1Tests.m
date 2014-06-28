fc77ecd471fbc597bbeadd80d03a1d8ed074220f
Денис Раскин <denisraskin@MacBook-Air-Denis.local>
2014-06-28 17:15:39 +0400

    Initial Commit

---
+++ t1Tests/t1Tests.m
@@ -0,0 +1,34 @@
+//
+//  t1Tests.m
+//  t1Tests
+//
+//  Created by Денис Раскин on 28.06.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface t1Tests : XCTestCase
+
+@end
+
+@implementation t1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
