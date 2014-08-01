a891d0cf4684de198d0532f87e914604b7db4e27
Денис Раскин <denisraskin@MacBook-Air-Denis.local>
2014-08-01 10:35:48 +0400

    1

--- t1/ViewController.h
+++ t1/ViewController.h
@@ -8,6 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <NSXMLParserDelegate>
+{
+    NSMutableData *rssData;
+    NSMutableArray *news;
+    NSString *currentElement;
+    NSMutableString *currentTitle;
+    NSMutableString *pubDate;
+    NSMutableString *about;
+    NSMutableString *pict;
+    UIImageView *imm;
+    NSString *corrURl;
+    NSArray *array4news;
+    
+    NSMutableArray *fArr;
+    
+}
 
+@property (nonatomic, retain) NSMutableData *rssData;
+@property (nonatomic, retain) NSMutableArray *news;
+@property (nonatomic, retain) NSMutableString *currentTitle;
+@property (nonatomic, retain) NSString *currentElement;
+@property (nonatomic, retain) NSMutableString *pubDate;
+@property (nonatomic, retain) UIViewController *detailViewController;
+@property (nonatomic, retain) NSMutableString *about;
+@property (nonatomic, retain) NSMutableString *pict;
+@property (nonatomic, retain) UIImageView *imm;
+@property (nonatomic, retain) NSString *corrURl;
+
+@property (nonatomic, retain) NSArray *array4news;
+@property (nonatomic, retain) NSMutableArray *fArr;
 @end
--- t1/ViewController.m
+++ t1/ViewController.m
@@ -14,16 +14,398 @@
 
 @implementation ViewController
 
+@synthesize rssData;
+@synthesize news;
+@synthesize currentTitle;
+@synthesize currentElement;
+@synthesize pubDate;
+@synthesize detailViewController;
+@synthesize about;
+@synthesize pict;
+@synthesize imm;
+@synthesize corrURl;
+@synthesize array4news;
+
+@synthesize fArr;
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.title=@"News";
+    
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    NSURL *url=[NSURL URLWithString:@"http://news.yandex.ru/world.rss"];
+    NSURLRequest *theRequest=[NSURLRequest requestWithURL:url
+                                              cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                          timeoutInterval:60.0];
+    NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+    if (theConnection)
+    {
+        self.rssData=[NSMutableData data];
+    } else
+    {
+        NSLog(@"fail :(");
+    }
+    //[theConnection release];
+    
+    
+    
+    
+    
 }
+
+
+-(void) connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [rssData appendData:data ];
+}
+
+
+-(void) connectionDidFinishLoading:(NSURLConnection *)connection {
+    NSString *result = [[NSString alloc] initWithData:rssData encoding:NSUTF8StringEncoding];
+    //NSLog(@"%@", result);
+    //   [result release];
+    
+    self.news=[NSMutableArray array];
+    NSXMLParser *rssParser=[[NSXMLParser alloc] initWithData:rssData];
+    rssParser.delegate=self;
+    [rssParser parse];
+    //  [rssParser release];
+}
+
+-(void) parser:(NSXMLParser *)parser
+didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict {
+    self.currentElement=elementName;
+    if ([elementName isEqualToString:@"item"]) {
+        self.currentTitle=[NSMutableString string];
+        self.pubDate =[NSMutableString string];
+        self.about =[NSMutableString string];
+        self.pict = [NSMutableString string];
+        
+    }
+}
+
+
+-(void) parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
+    if ([currentElement isEqualToString:@"title"])
+    {
+        [currentTitle appendString:string];
+    } else if ([currentElement isEqualToString:@"pubDate"]) {
+        [pubDate appendString:string];
+    }
+    else if ([currentElement isEqualToString:@"description"]) {
+        [about appendString:string];
+    }
+    else if ([currentElement isEqualToString:@"image"]) {
+        [pict appendString:string];
+        
+        
+    }
+    
+}
+
+
+-(void) parser:(NSXMLParser *)parser
+ didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
+    if ([elementName isEqualToString:@"item"]) {
+        NSDictionary *newsItem =[NSDictionary dictionaryWithObjectsAndKeys:
+                                 currentTitle, @"title",
+                                 pubDate, @"pubDate",
+                                 about, @"description",
+                                 pict, @"image",nil];
+        self.array4news = [NSMutableArray array];
+        
+  //      NSString *secondString  = [[NSString alloc] initWithData:[newsItem objectForKey:@"description"] encoding:NSUTF8StringEncoding];
+      //  NSData *d = [[NSData alloc] initWithData:[newsItem objectForKey:@"description" ]];
+        /*        fArr = [[NSMutableArray alloc] init];
+        
+        NSString *sStr = [[NSString alloc] initWithString:about] ;
+       // NSLog(@"%@", sStr);
+        array4news = [sStr componentsSeparatedByString:@" "];
+       // NSLog(@"%@",[ array4news objectAtIndex:3]);
+        for (int i=0; i<[array4news count]; i++) {
+            [fArr addObject:[array4news objectAtIndex:i]];// I вариант
+        }
+        NSLog(@"%@", [fArr objectAtIndex:3]);*/
+        [news addObject:newsItem];
+        self.currentTitle=nil;
+        self.pubDate=nil;
+        self.currentElement =nil;
+        self.about=nil;
+        self.pict=nil;
+        
+    }
+   // NSLog(@"%@", [fArr objectAtIndex:3]);
+   // NSLog(@"==%@", [[news objectAtIndex:3]objectForKey:@"description"]);// II вариант
+   
+}
+
+
+/*-(void) gettingFinalArray: (NSDictionary *)dictionary {
+    array4news = [[NSArray alloc] init];
+    int j=0;
+    for (int i = 0; i<[news count]; i++) {
+        NSString *sStr = [[NSString alloc] initWithString:about] ;
+        array4news = [sStr componentsSeparatedByString:@" "];
+   
+        for (j=j; j<j+[array4news count]; j++) {
+            [fArr addObject:[array4news objectAtIndex:j]];
+        }
+        
+        j=j+[array4news count];
+    }
+    NSLog(@"%@", fArr);
+}
+
+*/
+
+
+-(void)parserDidEndDocument:(NSXMLParser *)parser {
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+  //  NSLog(@"==%@", [[news objectAtIndex:3]objectForKey:@"description"]);// II вариант
+    
+    array4news = [[NSArray alloc] init];
+    fArr = [[NSMutableArray alloc] init];
+    int j=0;
+    for (int i = 0; i<[news count]; i++) {
+    
+        NSString *sStr = [[NSString alloc] initWithString:[[news objectAtIndex:i] objectForKey: @"description"]] ;
+        array4news = [sStr componentsSeparatedByString:@" "];
+        
+        
+        for (j=0; j<[array4news count]; j++) {
+            [fArr addObject:[array4news objectAtIndex:j]];
+         //   NSLog(@"%@", [array4news objectAtIndex:j]);//array4n 28obj
+           // NSLog(@"%d", [fArr count]); //~519
+        }
+        
+       // j=j+[array4news count];
+    }
+   // NSLog(@"%@", [array4news objectAtIndex:3]);
+   // NSLog(@"%d", [fArr count]); //~519
+
+    
+    
+    
+    /* ftw
+    NSMutableArray *countingArray = [[NSMutableArray alloc] init];
+    int c=0;
+    for (int k=0; k<[fArr count]; k++) {
+        NSString *str1 = [[NSString alloc] init];
+        str1 = [fArr objectAtIndex:k];
+       // str1=@"в";
+        
+        
+        for ( int l=1; l<[fArr count]-1; l++) {
+            
+            if ([fArr objectAtIndex:l]==str1) {
+                c=c+1;
+               // NSLog(@"%d", c);
+               // NSLog(@"%d", l);
+            }
+            NSNumber *anumber = [NSNumber numberWithInteger:c];
+            [countingArray addObject:anumber];
+            NSLog(@"%@", countingArray);
+        }
+        
+    }
+    
+    */
+    
+    
+    
+
+    for (int k=0; k<[fArr count]; k++) {
+        
+        NSString *str1 = [[NSString alloc] init];
+        str1 = [fArr objectAtIndex:k];
+        NSMutableArray *array4words = [[NSMutableArray alloc] init];
+        [array4words addObject:str1];
+        int c=0;
+        
+        NSMutableArray *countingArray = [[NSMutableArray alloc] init];
+        
+        for (int l=0; l<[array4words count]; l++) {
+ 
+            
+            
+            
+    
+            
+/********************
+ исправить подсчет с
+ посмотреть как должно изменяться l относительно k и что вообще за хрень
+ кодировка (лайфхак: NSLog(@"%@", [array4words objectAtIndex:l]); )
+ ********************/
+ 
+            
+            
+           NSLog(@"%@", [array4words objectAtIndex:l]);
+            NSLog(@"%d", l);
+           
+            if ([array4words objectAtIndex:l]!=str1) {
+                [array4words addObject:[fArr objectAtIndex:l]];
+                k++;
+               
+            }
+                else {
+                    k++;
+                    c=c+1;
+                }
+            NSNumber *anumber = [NSNumber numberWithInteger:c];
+           [countingArray addObject:anumber];
+            NSLog (@"%@", countingArray);
+            l++;
+            }
+       
+        
+        }
+    
+}
+    
+
+
+-(void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError
+{NSLog (@"%@", parseError);
+}
+
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [news count];
+    
+}
+
+
+
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+/*
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:CellIdentifier] ; //]autorelease];
+        //   ImagesForTables *immag1=[[[ImagesForTables alloc]init] autorelease];
+        //   LabelsForTables *lab1=[[[LabelsForTables alloc] initWithFrame:CGRectMake(60, 2, 350, 15)] autorelease];
+        //   LabelsForTables *lab2=[[[LabelsForTables alloc] initWithFrame:CGRectMake(60, 20 , 350, 15)] autorelease];
+        
+        //   lab1.tag=12;
+        //  lab2.tag=13;
+        
+        // [immag loadImageFromURL];
+        
+        //immag.imageView=imm;
+        
+        // immag1.tag=10;
+        
+        //NSData *dat=[NSData dataWithContentsOfURL:url];
+        //imm=[UIImage imageWithData:dat];
+        //cell.imageView.image=imm;
+        
+        //  [cell.contentView addSubview:immag1];
+        //  [cell.contentView addSubview:lab1];
+        //  [cell.contentView addSubview:lab2];
+    }
+    
+    NSDictionary *newsItem = [news objectAtIndex:indexPath.row];
+    
+    // cell.textLabel.text = [newsItem objectForKey:@"title"];
+    
+    // cell.detailTextLabel.text = [newsItem objectForKey:@"pubDate"];
+    
+    //  LabelsForTables *lab=(LabelsForTables *)[cell.contentView viewWithTag:12];
+    //  lab.text=[newsItem objectForKey:@"title"];
+    
+    //  [cell.contentView bringSubviewToFront:lab];
+    
+    
+    //  LabelsForTables *labe=(LabelsForTables *)[cell.contentView viewWithTag:13];
+    //  labe.text=[newsItem objectForKey:@"pubDate"];
+    
+    
+    //  [cell.contentView bringSubviewToFront:labe];
+    
+    //NSLog(@"%@", lab.text);
+    
+    
+    NSString *str=[[news objectAtIndex:indexPath.row ] objectForKey:@"image"];//NSLog(@"%@", str);
+    
+    
+    NSString *imagelink=[NSString stringWithFormat:@"%@%@",@"http://www.imaladec.net", str];
+    
+    NSString *corrURl1 = [imagelink stringByReplacingOccurrencesOfString:@"\n\t\t    " withString:@""];
+    
+    
+    
+    NSURL *url=[NSURL URLWithString:corrURl1];
+    //  ImagesForTables *immag= (ImagesForTables *)[cell.contentView viewWithTag:10];
+    //  if (immag.imageView && immag.imageView.image) immag.imageView.image=nil;
+    
+    
+    //  [immag loadImageFromURL:url];
+    //  [cell.contentView bringSubviewToFront:immag];
+    
+    return cell;
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    //  NewsViewController *detailViewController1 = [[NewsViewController alloc]
+    //                                              initWithNibName:@"NewsViewController" bundle:nil];
+    //  detailViewController1.selectedNews = [news objectAtIndex:indexPath.row];
+    //   [self.navigationController pushViewController:detailViewController1 animated:YES];
+    //  [detailViewController1 release];
+    
+}
+
+*/
+
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error{
+    NSLog(@"%@", error);
+}
+
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
 
 - (void)didReceiveMemoryWarning
 {
+    // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    // Release any cached data, images, etc. that aren't in use.
 }
 
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc
+{
+    //[super dealloc];
+}
+
+
 @end
+
